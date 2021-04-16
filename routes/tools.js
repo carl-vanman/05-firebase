87052aff8275c09f85d941fb4dc23ecfb593bd69
carl-vanman <70966062+carl-vanman@users.noreply.github.com>
2021-04-16 19:05:12 +0200

    Koppla samman front med backend

--- routes/tools.js
+++ routes/tools.js
@@ -5,10 +5,33 @@ const express = require('express')
 const router = express.Router()
 
 // REST API
-router.get('/', (req, res) => {
-	console.log('/tools REST API');
-	res.send('/tools REST API')
+
+
+//GET /tools
+router.get('/', async (req, res) => {
+	// console.log('/tools REST API');
+	// res.send('/tools REST API')
+
+	const toolsRef = db.collection('tools')
+	const snapshot = await toolsRef.get()
+
+	if( snapshot.empty ){
+		res.send([])
+		return
+	}
+
+	let items = []
+	snapshot.forEach(doc => {
+		const data = doc.data()
+		data.id = doc.id
+		items.push( data )
+	})
+	res.send(items)
 })
 
+// GET /tools/:id
+// POST /tools
+// PUT /tools/:id
+// DELETE /tools/:id
 
 module.exports = router
